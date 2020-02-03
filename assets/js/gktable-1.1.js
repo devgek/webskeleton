09058c40f04b8629d0922996fb73733fdefd376d
Gerald Kahrer <Gerald.Kahrer@extern.wrwks.at>
2020-02-03 11:35:04 +0100

    first commit

---
+++ assets/js/gktable-1.1.js
@@ -0,0 +1,275 @@
+//GKTableObjects container holding all GKTable-objects of current site
+var GKTableObjects = [];
+//buttonColTemplate code template for creating the edit button column in each table row
+var buttonColTemplate = `
+<td class="gk-col-buttons">
+<div class="text-nowrap">
+<button type="button" class="btn btn-secondary gk-btn-edit" onclick="tableId.handleEdit(this);">Bearbeiten</button>
+<button type="button" class="btn btn-primary gk-btn-save btn-sm d-none" onclick="tableId.handleSave(this);">Speichern</button>
+<button type="button" class="btn btn-primary gk-btn-delete btn-sm d-none" onclick="tableId.handleDelete(this);">Löschen</button>
+<button type="button" class="btn btn-primary gk-btn-abort btn-sm d-none" onclick="tableId.handleAbort(this);">Abbrechen</button>
+</div> 
+</td>`;
+//buttonNewTemplate code template for creating the new button
+var buttonNewTemplate = `
+<button type="button" class="btn btn-secondary gk-btn-new" onclick="tableId.handleNew();">Neu</button>`;
+
+//initialize the GKTable objects after loading site
+$(document).ready(function() {
+  for (index = 0; index < GKTableObjects.length; ++index) {
+    GKTableObjects[index].onLoadDocument();
+  }
+});
+
+//GKTable constructor function
+//each GKTable must have a tableId, a callback function for saving row data and a callback function for deleting row data
+function GKTable(tableId, saveFunc, deleteFunc) {
+  this.tableId = tableId;
+  this.rowData = [];
+  this.saveFunc = saveFunc;
+  this.deleteFunc = deleteFunc;
+  this.root = $("table.gk-table[id=" + this.tableId + "]");
+  this.buttonCol = buttonColTemplate.replace(
+    new RegExp("tableId", "g"),
+    this.tableId
+  );
+  this.buttonNew = buttonNewTemplate.replace(
+    new RegExp("tableId", "g"),
+    this.tableId
+  );
+  this.toast = $(".toast[data-tableId=" + this.tableId + "]");
+
+  GKTableObjects.push(this);
+
+  this.onLoadDocument = function() {
+    //prepare gk-table editing
+    const $rows = $(this.root).find("tbody tr");
+    var theButtonCol = this.buttonCol;
+    $rows.each(function(index, element) {
+      $(element).addClass("gk-row-edit");
+      $(element).attr("data-persisted", "true");
+
+      const $cols = $(element).find("td");
+      $cols.each(function(i, c) {
+        $(c).addClass("gk-col-edit");
+      });
+
+      $(element).append(theButtonCol);
+    });
+
+    //compute the row index
+    this.reindexRows();
+
+    //add button new
+    $(".gk-btn-new-container[data-tableId=" + this.tableId + "]").prepend(
+      this.buttonNew
+    );
+
+    //init the toast
+    this.toast.toast((delay = 2000));
+  };
+
+  this.activateRowEditing = function(theRow) {
+    this.disableActions();
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "] button.gk-btn-edit")
+      .removeClass("d-inline")
+      .addClass("d-none");
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "] button.gk-btn-save")
+      .removeClass("d-none")
+      .addClass("d-inline");
+    if (this.isPersisted(theRow)) {
+      $(this.root)
+        .find(".gk-row-edit[data-linenr=" + theRow + "] button.gk-btn-delete")
+        .removeClass("d-none")
+        .addClass("d-inline");
+    }
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "] button.gk-btn-abort")
+      .removeClass("d-none")
+      .addClass("d-inline");
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "]")
+      .addClass("bg-warning");
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "]")
+      .find("td")
+      .prop("contenteditable", "true");
+
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "] td.gk-col-edit")[0]
+      .focus();
+  };
+
+  this.deactivateRowEditing = function(theRow) {
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "] button.gk-btn-edit")
+      .removeClass("d-none")
+      .addClass("d-inline");
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "] button.gk-btn-new")
+      .removeClass("d-none")
+      .addClass("d-inline");
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "] button.gk-btn-save")
+      .removeClass("d-inline")
+      .addClass("d-none");
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "] button.gk-btn-delete")
+      .removeClass("d-inline")
+      .addClass("d-none");
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "] button.gk-btn-abort")
+      .removeClass("d-inline")
+      .addClass("d-none");
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "]")
+      .removeClass("bg-warning");
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "]")
+      .find("td")
+      .prop("contenteditable", "false");
+    this.enableActions();
+  };
+
+  this.getRowData = function(theRow) {
+    const $cols = $(this.root).find(
+      ".gk-row-edit[data-linenr=" + theRow + "] td.gk-col-edit"
+    );
+    var rowData = [];
+    $cols.each(function(index, element) {
+      rowData.push($(element).text());
+    });
+
+    return rowData;
+  };
+
+  this.storeRowData = function(theRow) {
+    const $cols = $(this.root).find(
+      ".gk-row-edit[data-linenr=" + theRow + "] td.gk-col-edit"
+    );
+    var theArray = this.rowData;
+    $cols.each(function(index, element) {
+      theArray.push($(element).text());
+    });
+  };
+
+  this.restoreRowData = function(theRow) {
+    const $cols = $(this.root).find(
+      ".gk-row-edit[data-linenr=" + theRow + "] td.gk-col-edit"
+    );
+    var theArray = this.rowData;
+    $cols.each(function(index, element) {
+      $(element).text(theArray[index]);
+    });
+  };
+
+  this.addRow = function(theRow) {
+    const $lastRow = $(this.root).find("tbody tr.gk-row-edit:last");
+    var clonedRow = $lastRow.clone();
+    clonedRow.attr("data-linenr", theRow);
+    clonedRow.attr("data-persisted", "false");
+    clonedRow.find("td.gk-col-edit").empty();
+    $(this.root)
+      .find("tbody")
+      .append(clonedRow);
+  };
+
+  this.enableActions = function() {
+    $(this.root)
+      .find(".gk-btn-edit")
+      .prop("disabled", false);
+    $(this.root)
+      .find(".gk-btn-new")
+      .prop("disabled", false);
+  };
+
+  this.disableActions = function() {
+    $(this.root)
+      .find(".gk-btn-edit")
+      .prop("disabled", true);
+    $(this.root)
+      .find(".gk-btn-new")
+      .prop("disabled", true);
+  };
+
+  this.deleteRow = function(theRow) {
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "]")
+      .remove();
+    this.reindexRows();
+    this.enableActions();
+  };
+
+  this.setPersisted = function(theRow) {
+    $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "]")
+      .attr("data-persisted", "true");
+  };
+
+  this.isPersisted = function(theRow) {
+    var persisted = $(this.root)
+      .find(".gk-row-edit[data-linenr=" + theRow + "]")
+      .attr("data-persisted");
+    return persisted == "true";
+  };
+
+  this.reindexRows = function() {
+    const $rows = $(this.root).find("tbody tr.gk-row-edit");
+    $rows.each(function(index, element) {
+      $(element).attr("data-linenr", index + 1);
+    });
+  };
+
+  this.getRowFromParent = function(innerObject) {
+    return $(innerObject)
+      .parents("tr.gk-row-edit")
+      .attr("data-linenr");
+  };
+
+  this.handleNew = function() {
+    var rows = $(this.root).find(".gk-row-edit").length;
+    this.addRow(rows + 1);
+    this.activateRowEditing(rows + 1);
+  };
+
+  this.handleEdit = function(obj) {
+    var line = this.getRowFromParent(obj);
+    this.storeRowData(line);
+    this.activateRowEditing(line);
+  };
+
+  this.handleSave = function(obj) {
+    var line = this.getRowFromParent(obj);
+    var saved = this.saveFunc(line, this.getRowData(line));
+    if (saved) {
+      this.setPersisted(line);
+      this.deactivateRowEditing(line);
+    }
+  };
+
+  this.handleDelete = function(obj) {
+    var line = this.getRowFromParent(obj);
+    var deleted = this.deleteFunc(line);
+    if (deleted) {
+      this.deleteRow(line);
+    }
+  };
+
+  this.handleAbort = function(obj) {
+    var theRow = this.getRowFromParent(obj);
+    if (this.isPersisted(theRow)) {
+      this.restoreRowData(theRow);
+      this.deactivateRowEditing(theRow);
+    } else {
+      this.deleteRow(theRow);
+    }
+  };
+
+  this.showMessage = function(msg) {
+    const $toast = $(".toast[data-tableId=" + this.tableId + "]");
+    $toast.find("span.gk-toast-text").text(msg);
+    $toast.toast("show");
+  };
+}
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"sync"
+	"text/template"
+
+	"github.com/stretchr/objx"
+)
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+type templateData struct {
+	Host   string
+	Client string
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		if t.filename == "login.html" {
+			t.templ = template.Must(template.ParseFiles("./templates/" + t.filename))
+		} else {
+			t.templ = template.Must(template.ParseFiles("./templates/layout.html", "./templates/menu.html", "./templates/"+t.filename))
+		}
+	})
+
+	data := map[string]interface{}{
+		"Host":        r.Host,
+		"VersionInfo": "V1.0",
+	}
+	if authCookie, err := r.Cookie("webskeleton-auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
+
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() // parse the flags
+
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.Handle("/page1", MustAuth(&templateHandler{filename: "page1.html"}))
+	http.Handle("/page2", &templateHandler{filename: "page2.html"})
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "webskeleton-auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header().Set("Location", "/page1")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
+
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
+
+	// start the web server
+	log.Println("Starting webskeleton on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
+}
